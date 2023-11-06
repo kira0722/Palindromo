950aa904f92d662a1eb31c8bda2d2d40c723718b
kira0722 <samuel_quirama23211@elpoli.edu.co>
2023-11-06 17:28:13 -0500

    se crea la clase Student junto con sus atributos

--- src/main/java/com/example/palindromoces3/Modem/Student.java
+++ src/main/java/com/example/palindromoces3/Modem/Student.java
@@ -1,2 +1,58 @@
-package com.example.palindromoces3.Modem;public class Student {
+package com.example.palindromoces3.Modem;
+
+import com.example.palindromoces3.Servlets.MyServlet;
+
+public class Student {
+    public String nombre;
+    public int id;
+    private String cedula;
+
+    public Student(int id, String cedula, String nombre){
+        this.id = id;
+        this.cedula = cedula;
+        this.nombre = nombre;
+    }
+
+    public Student(String cedula){
+        this.cedula = cedula;
+    }
+
+    public Student() {
+
+    }
+
+
+    //-----------------------------------------------------------
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+
+    @Override
+    public String toString() {
+        return "el estudiante se llama: "+ this.nombre+ " su documento es: "+ this.cedula;
+
+    }
 }
